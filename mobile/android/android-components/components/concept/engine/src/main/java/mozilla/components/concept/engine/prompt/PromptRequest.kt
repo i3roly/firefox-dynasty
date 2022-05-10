mozilla-mobile/firefox-android

[components] Issue https://github.com/mozilla-mobile/android-components/issues/11249: Add support for save credit card prompt

android-components/components/concept/engine/src/main/java/mozilla/components/concept/engine/prompt/PromptRequest.kt
@@ -77,6 +77,18 @@ sealed class PromptRequest(
     ) : PromptRequest()
 
     /**
+     * Value type that represents a request for a save credit card prompt.
+     * @property creditCard the [CreditCard] to save or update.
+     * @property onDismiss callback to let the page know the user dismissed the dialog.
+     * @property onConfirm callback that is called when the user confirms the credit card selection.
+     */
+    data class SaveCreditCard(
+        val creditCard: CreditCard,
+        override val onDismiss: () -> Unit,
+        val onConfirm: (CreditCard) -> Unit
+    ) : PromptRequest(shouldDismissOnLoad = false), Dismissible
+
+    /**
      * Value type that represents a request for a select credit card prompt.
      * @property creditCards a list of [CreditCard]s to select from.
      * @property onDismiss callback to let the page know the user dismissed the dialog.

i3roly/firefox-dynasty

mobile/android/android-components/components/concept/engine/src/main/java/mozilla/components/concept/engine/prompt/PromptRequest.kt
@@ -94,6 +94,18 @@ sealed class PromptRequest(
     ) : PromptRequest()
 
     /**
+     * Value type that represents a request for a save credit card prompt.
+     * @property creditCard the [CreditCardEntry] to save or update.
+     * @property onConfirm callback that is called when the user confirms the save credit card request.
+     * @property onDismiss callback to let the page know the user dismissed the dialog.
+     */
+    data class SaveCreditCard(
+        val creditCard: CreditCardEntry,
+        val onConfirm: (CreditCardEntry) -> Unit,
+        override val onDismiss: () -> Unit
+    ) : PromptRequest(shouldDismissOnLoad = false), Dismissible
+
+    /**
      * Value type that represents a request for a select credit card prompt.
      * @property creditCards a list of [CreditCardEntry]s to select from.
      * @property onConfirm callback that is called when the user confirms the credit card selection.