chromium/chromium

commit 34dc1247e6267b54675f580862b909d927c5f124

Forbid script inside ContextLifecycleNotifier::ContextDestroyed().
The ContextDestroyed() callback is intended for general cleanup, not
as one last chance to run script. If script does execute inside this
callback, it might cause a detach-within-detach, which leads to classes
receiving multiple ContextDestroyed() callbacks and illegal use of
iterators in ContextLifecycleNotifier.

Fixed: 1244148
Change-Id: I34f48d5da818653a676d435ee0c1b9c5df9c0431
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133987
Reviewed-by: Kentaro Hara <haraken@chromium.org>
Commit-Queue: Nate Chapin <japhet@chromium.org>
Cr-Commit-Position: refs/heads/main@{#919809}

third_party/blink/renderer/core/streams/readable_stream.cc
@@ -1750,6 +1750,9 @@ v8::Local<v8::Promise> ReadableStream::Cancel(ScriptState* script_state,
 }
 
 void ReadableStream::Close(ScriptState* script_state, ReadableStream* stream) {
+  if (ExecutionContext::From(script_state)->IsContextDestroyed())
+    return;
+
   // https://streams.spec.whatwg.org/#readable-stream-close
   // 1. Assert: stream.[[state]] is "readable".
   CHECK_EQ(stream->state_, kReadable);

third_party/blink/renderer/modules/webcodecs/image_decoder_external.cc
@@ -360,7 +360,6 @@ void ImageDecoderExternal::close() {
   auto* exception = MakeGarbageCollected<DOMException>(
       DOMExceptionCode::kAbortError,
       failed_ ? "Aborted by close." : "Aborted by failure.");
-  reset(exception);
 
   // Failure cases should have already rejected the tracks ready promise.
   if (!failed_ && decoder_ && tracks_->IsEmpty())
@@ -371,7 +370,11 @@ void ImageDecoderExternal::close() {
 
   if (consumer_)
     consumer_->Cancel();
+  CloseInternal(exception);
+}
 
+void ImageDecoderExternal::CloseInternal(DOMException* exception) {
+  reset(exception);
   weak_factory_.InvalidateWeakPtrs();
   pending_metadata_requests_ = 0;
   consumer_ = nullptr;
@@ -439,9 +442,11 @@ void ImageDecoderExternal::ContextDestroyed() {
   // WeakPtrs need special consideration when used with a garbage collected
   // type; they must be invalidated ahead of finalization.
   //
-  // We also need to ensure that no further WeakPtrs are created, so close() the
+  // We also need to ensure that no further WeakPtrs are created, so close the
   // decoder at this point to prevent further operation.
-  close();
+  auto* exception = MakeGarbageCollected<DOMException>(
+      DOMExceptionCode::kAbortError, "Aborted by close.");
+  CloseInternal(exception);
 
   DCHECK(!weak_factory_.HasWeakPtrs());
   DCHECK(!decode_weak_factory_.HasWeakPtrs());

third_party/blink/renderer/modules/webcodecs/image_decoder_external.h
@@ -84,6 +84,7 @@ class MODULES_EXPORT ImageDecoderExternal final
   void OnMetadata(ImageDecoderCore::ImageMetadata metadata);
 
   void SetFailed();
+  void CloseInternal(DOMException*);
 
   Member<ScriptState> script_state_;
 

third_party/blink/renderer/modules/webgpu/gpu_device.cc
@@ -194,6 +194,8 @@ void GPUDevice::OnLogging(WGPULoggingType loggingType, const char* message) {
 }
 
 void GPUDevice::OnDeviceLostError(const char* message) {
+  if (!GetExecutionContext())
+    return;
   AddConsoleWarning(message);
 
   if (lost_property_->GetState() == LostProperty::kPending) {

third_party/blink/renderer/platform/context_lifecycle_notifier.cc
@@ -4,6 +4,7 @@
 
 #include "third_party/blink/renderer/platform/context_lifecycle_notifier.h"
 
+#include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
 #include "third_party/blink/renderer/platform/context_lifecycle_observer.h"
 
 namespace blink {
@@ -27,6 +28,7 @@ void ContextLifecycleNotifier::RemoveContextLifecycleObserver(
 }
 
 void ContextLifecycleNotifier::NotifyContextDestroyed() {
+  ScriptForbiddenScope forbid_script;
   observers_.ForEachObserver([](ContextLifecycleObserver* observer) {
     observer->NotifyContextDestroyed();
   });

third_party/blink/web_tests/fast/loader/detach-inside-getUserMedia-error-callback-crash.html
@@ -1,0 +1,16 @@
+<!DOCTYPE html>
+<script src='../../resources/testharness.js'></script>
+<script src='../../resources/testharnessreport.js'></script>
+<body>
+<script>
+test(() => {
+  let child = document.createElement('iframe');
+  document.body.appendChild(child);
+  let grandchild = document.createElement('iframe');
+  child.contentDocument.body.appendChild(grandchild);
+  grandchild.contentWindow.navigator.getUserMedia({video:undefined, audio:true}, () => {}, () => grandchild.remove());
+  child.remove();
+  assert_true(true, 'Pass if no crash.');
+}, "Regression test for https://crbug.com/1244148");
+</script>
+</body>

i3roly/firefox-dynasty

commit 3d2dfb6e77a53abe2487a5d308a9e1cac4c949c7

Bug 1769672 [wpt PR 34088] - [ImageDecoder] Ensure ReadableStream consumer is cancelled., a=testonly
Automatic update from web-platform-tests
[ImageDecoder] Ensure ReadableStream consumer is cancelled.

34dc1247e6267b54675f580862b909d927c5f124 mistakenly removed a call
to ReadableStreamBytesConsumer::Cancel() during some shutdown paths.

The consumer must be cancelled in all cases after activation or it
can issue calls back into the client we register after destruction.

R=japhet

Fixed: 1319714
Change-Id: I4495e470484ac66564d5ee271cd059bb7021117b
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629902
Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
Reviewed-by: Nate Chapin <japhet@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1004567}

--

wpt-commits: dd05a4454ef3e76677d1191823b34420f3ea9355
wpt-pr: 34088

testing/web-platform/tests/webcodecs/image-decoder-disconnect-readable-stream-crash.https.html
@@ -1,0 +1,12 @@
+<title>Test ImageDecoder destruction w/ ReadableStream doesn't crash.</title>
+<body>
+<script>
+let iframe = document.createElement('iframe');
+document.body.appendChild(iframe);
+let decoder = new iframe.contentWindow.ImageDecoder({
+    data: new Blob(['blob']).stream(),
+    type: 'image/jpeg',
+});
+document.querySelector('body').remove();
+</script>
+</body>