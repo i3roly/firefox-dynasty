mozilla-mobile/firefox-android

Bug 1804258 - Prevent overlapping of cancel requests for history reads
With more search providers querying the same history storage but only after
they try to cancel the in-progress queries it is possible for the cancel
requests to overlap and one search provider to cancel the queries of another.

To mitigate this a new method is available which will use the current input to
query the storage for as a way to drop new cancel requests for the same input.

android-components/components/browser/storage-sync/src/main/java/mozilla/components/browser/storage/sync/PlacesStorage.kt
@@ -43,6 +43,12 @@ abstract class PlacesStorage(
         @VisibleForTesting internal set
     private val storageDir by lazy { context.filesDir }
 
+    /**
+     * Cache of the last value with which [cancelReads] was called.
+     * Used to check whether a new call to [cancelReads] should trigger a cancellation or not.
+     */
+    private var lastCancelledQuery = ""
+
     abstract val logger: Logger
 
     internal open val places: Connection by lazy {
@@ -91,6 +97,23 @@ abstract class PlacesStorage(
     override fun cancelReads() {
         interruptCurrentReads()
         readScope.coroutineContext.cancelChildren()
+    }
+
+    /**
+     * Cleans up pending read operations of a specific query.
+     *
+     * @param queryToCancel Previous query to cancel reads for.
+     * Calling cancel multiple times for the same query has effect only the first time.
+     * Use this in scenarios where the same instance is used in multiple scenarios to prevent cases
+     * in which a general cancel operation for one scenario cancels other reads for the same query.
+     * If the value is an empty string all current reads are immediately cancelled.
+     */
+    override fun cancelReadsOfQuery(queryToCancel: String) {
+        if (queryToCancel.isEmpty() || lastCancelledQuery != queryToCancel) {
+            lastCancelledQuery = queryToCancel
+            interruptCurrentReads()
+            readScope.coroutineContext.cancelChildren()
+        }
     }
 
     /**

android-components/components/browser/storage-sync/src/test/java/mozilla/components/browser/storage/sync/PlacesStorageTest.kt
@@ -14,6 +14,7 @@ import mozilla.components.support.test.mock
 import org.junit.Test
 import org.mockito.Mockito.doAnswer
 import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import kotlin.coroutines.CoroutineContext
 
@@ -52,6 +53,25 @@ class PlacesStorageTest {
 
         verify(storage.reader).interrupt()
         verify(storage.readScope.coroutineContext).cancelChildren()
+    }
+
+    @Test
+    fun `GIVEN a specific query WHEN a call is made to clean all reads THEN they are cancelled only if the query is different from the previous call`() {
+        storage.readScope = mock {
+            doReturn(mock<CoroutineContext>()).`when`(this).coroutineContext
+        }
+
+        storage.cancelReadsOfQuery("test")
+        verify(storage.reader, times(1)).interrupt()
+        verify(storage.readScope.coroutineContext, times(1)).cancelChildren()
+
+        storage.cancelReadsOfQuery("test")
+        verify(storage.reader, times(1)).interrupt()
+        verify(storage.readScope.coroutineContext, times(1)).cancelChildren()
+
+        storage.cancelReadsOfQuery("tset")
+        verify(storage.reader, times(2)).interrupt()
+        verify(storage.readScope.coroutineContext, times(2)).cancelChildren()
     }
 
     @Test

android-components/components/feature/awesomebar/src/main/java/mozilla/components/feature/awesomebar/provider/CombinedHistorySuggestionProvider.kt
@@ -60,8 +60,8 @@ class CombinedHistorySuggestionProvider(
     }
 
     override suspend fun onInputChanged(text: String): List<AwesomeBar.Suggestion> = coroutineScope {
-        historyStorage.cancelReads()
-        historyMetadataStorage.cancelReads()
+        historyStorage.cancelReadsOfQuery(text)
+        historyMetadataStorage.cancelReadsOfQuery(text)
 
         if (text.isBlank()) {
             return@coroutineScope emptyList()

android-components/components/feature/awesomebar/src/main/java/mozilla/components/feature/awesomebar/provider/HistoryMetadataSuggestionProvider.kt
@@ -53,7 +53,7 @@ class HistoryMetadataSuggestionProvider(
     }
 
     override suspend fun onInputChanged(text: String): List<AwesomeBar.Suggestion> {
-        historyStorage.cancelReads()
+        historyStorage.cancelReadsOfQuery(text)
 
         if (text.isNullOrBlank()) {
             return emptyList()

android-components/components/feature/awesomebar/src/main/java/mozilla/components/feature/awesomebar/provider/HistoryStorageSuggestionProvider.kt
@@ -54,7 +54,7 @@ class HistoryStorageSuggestionProvider(
     }
 
     override suspend fun onInputChanged(text: String): List<AwesomeBar.Suggestion> {
-        historyStorage.cancelReads()
+        historyStorage.cancelReadsOfQuery(text)
 
         if (text.isEmpty()) {
             return emptyList()

android-components/components/feature/awesomebar/src/test/java/mozilla/components/feature/awesomebar/provider/HistoryMetadataSuggestionProviderTest.kt
@@ -64,7 +64,8 @@ class HistoryMetadataSuggestionProviderTest {
 
         provider.onInputChanged("")
 
-        verify(provider.historyStorage).cancelReads()
+        verify(provider.historyStorage, never()).cancelReads()
+        verify(provider.historyStorage).cancelReadsOfQuery("")
     }
 
     @Test
@@ -76,7 +77,8 @@ class HistoryMetadataSuggestionProviderTest {
 
         provider.onInputChanged("moz")
 
-        orderVerifier.verify(provider.historyStorage).cancelReads()
+        orderVerifier.verify(provider.historyStorage, never()).cancelReads()
+        orderVerifier.verify(provider.historyStorage).cancelReadsOfQuery("moz")
         orderVerifier.verify(provider.historyStorage).queryHistoryMetadata(eq("moz"), anyInt())
     }
 

android-components/components/feature/awesomebar/src/test/java/mozilla/components/feature/awesomebar/provider/HistoryStorageSuggestionProviderTest.kt
@@ -57,7 +57,8 @@ class HistoryStorageSuggestionProviderTest {
 
         provider.onInputChanged("")
 
-        verify(provider.historyStorage).cancelReads()
+        verify(provider.historyStorage, never()).cancelReads()
+        verify(provider.historyStorage).cancelReadsOfQuery("")
     }
 
     @Test
@@ -70,7 +71,8 @@ class HistoryStorageSuggestionProviderTest {
 
         provider.onInputChanged("moz")
 
-        orderVerifier.verify(provider.historyStorage).cancelReads()
+        orderVerifier.verify(provider.historyStorage, never()).cancelReads()
+        orderVerifier.verify(provider.historyStorage).cancelReadsOfQuery("moz")
         orderVerifier.verify(provider.historyStorage).getSuggestions(eq("moz"), anyInt())
     }
 

android-components/components/feature/session/src/test/java/mozilla/components/feature/session/HistoryDelegateTest.kt
@@ -189,5 +189,9 @@ class HistoryDelegateTest {
         override fun cancelReads() {
             fail()
         }
+
+        override fun cancelReadsOfQuery(queryToCancel: String) {
+            fail()
+        }
     }
 }

i3roly/firefox-dynasty

mobile/android/android-components/components/browser/storage-sync/src/main/java/mozilla/components/browser/storage/sync/PlacesStorage.kt
@@ -43,6 +43,12 @@ abstract class PlacesStorage(
         @VisibleForTesting internal set
     private val storageDir by lazy { context.filesDir }
 
+    /**
+     * Cache of the last value with which [cancelReads] was called.
+     * Used to check whether a new call to [cancelReads] should trigger a cancellation or not.
+     */
+    private var lastCancelledQuery = ""
+
     abstract val logger: Logger
 
     internal open val places: Connection by lazy {
@@ -91,6 +97,23 @@ abstract class PlacesStorage(
     override fun cancelReads() {
         interruptCurrentReads()
         readScope.coroutineContext.cancelChildren()
+    }
+
+    /**
+     * Cleans up pending read operations of a specific query.
+     *
+     * @param nextQuery Previous query to cancel reads for.
+     * Calling cancel multiple times for the same query has effect only the first time.
+     * Use this in scenarios where the same instance is used in multiple scenarios to prevent cases
+     * in which a general cancel operation for one scenario cancels other reads for the same query.
+     * If the value is an empty string all current reads are immediately cancelled.
+     */
+    override fun cancelReads(nextQuery: String) {
+        if (nextQuery.isEmpty() || lastCancelledQuery != nextQuery) {
+            lastCancelledQuery = nextQuery
+            interruptCurrentReads()
+            readScope.coroutineContext.cancelChildren()
+        }
     }
 
     /**

mobile/android/android-components/components/browser/storage-sync/src/test/java/mozilla/components/browser/storage/sync/PlacesStorageTest.kt
@@ -14,6 +14,7 @@ import mozilla.components.support.test.mock
 import org.junit.Test
 import org.mockito.Mockito.doAnswer
 import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import kotlin.coroutines.CoroutineContext
 
@@ -52,6 +53,25 @@ class PlacesStorageTest {
 
         verify(storage.reader).interrupt()
         verify(storage.readScope.coroutineContext).cancelChildren()
+    }
+
+    @Test
+    fun `GIVEN a specific query WHEN a call is made to clean all reads THEN they are cancelled only if the query is different from the previous call`() {
+        storage.readScope = mock {
+            doReturn(mock<CoroutineContext>()).`when`(this).coroutineContext
+        }
+
+        storage.cancelReads("test")
+        verify(storage.reader, times(1)).interrupt()
+        verify(storage.readScope.coroutineContext, times(1)).cancelChildren()
+
+        storage.cancelReads("test")
+        verify(storage.reader, times(1)).interrupt()
+        verify(storage.readScope.coroutineContext, times(1)).cancelChildren()
+
+        storage.cancelReads("tset")
+        verify(storage.reader, times(2)).interrupt()
+        verify(storage.readScope.coroutineContext, times(2)).cancelChildren()
     }
 
     @Test

mobile/android/android-components/components/feature/awesomebar/src/main/java/mozilla/components/feature/awesomebar/provider/CombinedHistorySuggestionProvider.kt
@@ -60,8 +60,8 @@ class CombinedHistorySuggestionProvider(
     }
 
     override suspend fun onInputChanged(text: String): List<AwesomeBar.Suggestion> = coroutineScope {
-        historyStorage.cancelReads()
-        historyMetadataStorage.cancelReads()
+        historyStorage.cancelReads(text)
+        historyMetadataStorage.cancelReads(text)
 
         if (text.isBlank()) {
             return@coroutineScope emptyList()

mobile/android/android-components/components/feature/awesomebar/src/main/java/mozilla/components/feature/awesomebar/provider/HistoryMetadataSuggestionProvider.kt
@@ -53,7 +53,7 @@ class HistoryMetadataSuggestionProvider(
     }
 
     override suspend fun onInputChanged(text: String): List<AwesomeBar.Suggestion> {
-        historyStorage.cancelReads()
+        historyStorage.cancelReads(text)
 
         if (text.isNullOrBlank()) {
             return emptyList()

mobile/android/android-components/components/feature/awesomebar/src/main/java/mozilla/components/feature/awesomebar/provider/HistoryStorageSuggestionProvider.kt
@@ -54,7 +54,7 @@ class HistoryStorageSuggestionProvider(
     }
 
     override suspend fun onInputChanged(text: String): List<AwesomeBar.Suggestion> {
-        historyStorage.cancelReads()
+        historyStorage.cancelReads(text)
 
         if (text.isEmpty()) {
             return emptyList()

mobile/android/android-components/components/feature/awesomebar/src/test/java/mozilla/components/feature/awesomebar/provider/HistoryMetadataSuggestionProviderTest.kt
@@ -64,7 +64,8 @@ class HistoryMetadataSuggestionProviderTest {
 
         provider.onInputChanged("")
 
-        verify(provider.historyStorage).cancelReads()
+        verify(provider.historyStorage, never()).cancelReads()
+        verify(provider.historyStorage).cancelReads("")
     }
 
     @Test
@@ -76,7 +77,8 @@ class HistoryMetadataSuggestionProviderTest {
 
         provider.onInputChanged("moz")
 
-        orderVerifier.verify(provider.historyStorage).cancelReads()
+        orderVerifier.verify(provider.historyStorage, never()).cancelReads()
+        orderVerifier.verify(provider.historyStorage).cancelReads("moz")
         orderVerifier.verify(provider.historyStorage).queryHistoryMetadata(eq("moz"), anyInt())
     }
 

mobile/android/android-components/components/feature/awesomebar/src/test/java/mozilla/components/feature/awesomebar/provider/HistoryStorageSuggestionProviderTest.kt
@@ -57,7 +57,8 @@ class HistoryStorageSuggestionProviderTest {
 
         provider.onInputChanged("")
 
-        verify(provider.historyStorage).cancelReads()
+        verify(provider.historyStorage, never()).cancelReads()
+        verify(provider.historyStorage).cancelReads("")
     }
 
     @Test
@@ -70,7 +71,8 @@ class HistoryStorageSuggestionProviderTest {
 
         provider.onInputChanged("moz")
 
-        orderVerifier.verify(provider.historyStorage).cancelReads()
+        orderVerifier.verify(provider.historyStorage, never()).cancelReads()
+        orderVerifier.verify(provider.historyStorage).cancelReads("moz")
         orderVerifier.verify(provider.historyStorage).getSuggestions(eq("moz"), anyInt())
     }
 

mobile/android/android-components/components/feature/session/src/test/java/mozilla/components/feature/session/HistoryDelegateTest.kt
@@ -189,5 +189,9 @@ class HistoryDelegateTest {
         override fun cancelReads() {
             fail()
         }
+
+        override fun cancelReads(nextQuery: String) {
+            fail()
+        }
     }
 }