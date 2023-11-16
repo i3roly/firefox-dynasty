PierceLBrooks/firefox-android

Bug 1861058 - Translations UI Make navigating from one bottom sheet to another more smooth.

fenix/app/src/main/java/org/mozilla/fenix/translations/TranslationSettingsFragment.kt
@@ -43,9 +43,10 @@ class TranslationSettingsFragment : Fragment(), UserInteractionHandler {
     }
 
     override fun onBackPressed(): Boolean {
-        findNavController().popBackStack()
         findNavController().navigate(
-            TranslationsDialogFragmentDirections.actionGlobalToTranslationOptionsDialogFragment(),
+            TranslationSettingsFragmentDirections.actionTranslationSettingsFragmentTranslationsDialogFragment(
+                TranslationsDialogAccessPoint.TranslationsOptions,
+            ),
         )
         return true
     }

i3roly/firefox-dynasty

mobile/android/fenix/app/src/main/java/org/mozilla/fenix/translations/TranslationSettingsFragment.kt
@@ -43,9 +43,10 @@ class TranslationSettingsFragment : Fragment(), UserInteractionHandler {
     }
 
     override fun onBackPressed(): Boolean {
-        findNavController().popBackStack()
         findNavController().navigate(
-            TranslationsDialogFragmentDirections.actionGlobalToTranslationOptionsDialogFragment(),
+            TranslationSettingsFragmentDirections.actionTranslationSettingsFragmentToTranslationsDialogFragment(
+                TranslationsDialogAccessPoint.TranslationsOptions,
+            ),
         )
         return true
     }