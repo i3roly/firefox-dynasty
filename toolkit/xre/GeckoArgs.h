3ab114bd853398a7974fd77a6e68fab6ad97feeb
broly <broly@mac.com>
2024-10-19 18:32:12 -0600

    Backed out changeset 8253fd9c090d (BUG 191704). HURTS BACKWARDS COMPATIBILITY
    destroys the contentview from working on 10.8 and 10.7, probably newer versions too.
    
    
    commit link: https://hg.mozilla.org/mozilla-unified/rev/8253fd9c090de81dc50968cbc3999f509aaa2bd7
    
    this was the commit note:
    
    Bug 1917904 - Support passing mach ports into new processes, r=ipc-reviewers,jld
    
    This builds on the support from bug 1440207 to allow passing mach ports into
    new content processes. This is done using the existing initial mach IPC already
    used to transfer a mach task port to the parent process on child process
    creation.
    --

--- toolkit/xre/GeckoArgs.h
+++ toolkit/xre/GeckoArgs.h
@@ -23,9 +23,6 @@ namespace geckoargs {
 struct ChildProcessArgs {
   std::vector<std::string> mArgs;
   std::vector<UniqueFileHandle> mFiles;
-#ifdef XP_DARWIN
-  std::vector<UniqueMachSendRight> mSendRights;
-#endif
 };
 
 #ifdef XP_UNIX
@@ -37,16 +34,6 @@ void SetPassedFileHandles(std::vector<UniqueFileHandle>&& aFiles);
 // Add the file handles from a ChildProcessArgs to a fdsToRemap table.
 void AddToFdsToRemap(const ChildProcessArgs& aArgs,
                      std::vector<std::pair<int, int>>& aFdsToRemap);
-#endif
-
-#ifdef XP_DARWIN
-// Size of the internal static array of mach send rights. This acts as a limit
-// to the number of mach send rights which can be passed on the command line.
-constexpr size_t kMaxPassedMachSendRights = 10;
-
-// Fill the internal static array with the mach send rights which were passed
-// from the parent process.
-void SetPassedMachSendRights(std::vector<UniqueMachSendRight>&& aSendRights);
 #endif
 
 template <typename T>
@@ -119,12 +106,6 @@ template <>
 Maybe<UniqueFileHandle> CommandLineArg<UniqueFileHandle>::GetCommon(
     const char* aMatch, int& aArgc, char** aArgv, const CheckArgFlag aFlags);
 
-#ifdef XP_DARWIN
-template <>
-Maybe<UniqueMachSendRight> CommandLineArg<UniqueMachSendRight>::GetCommon(
-    const char* aMatch, int& aArgc, char** aArgv, const CheckArgFlag aFlags);
-#endif
-
 /// Put()
 
 template <>
@@ -162,13 +143,6 @@ template <>
 void CommandLineArg<UniqueFileHandle>::PutCommon(const char* aName,
                                                  UniqueFileHandle aValue,
                                                  ChildProcessArgs& aArgs);
-
-#ifdef XP_DARWIN
-template <>
-void CommandLineArg<UniqueMachSendRight>::PutCommon(const char* aName,
-                                                    UniqueMachSendRight aValue,
-                                                    ChildProcessArgs& aArgs);
-#endif
 
 #if defined(__GNUC__)
 #  pragma GCC diagnostic push
