d150365a8e882a6d3a3602b0f436d08eb4a926cb
broly <broly@mac.com>
2025-01-11 16:18:27 -0700

    rlbox touchup (release)

--- dom/media/RLBoxSoundTouch.h
+++ dom/media/RLBoxSoundTouch.h
@@ -10,13 +10,11 @@
 
 // Load general firefox configuration of RLBox
 #include "mozilla/rlbox/rlbox_config.h"
-#undef RLBOX_WASM2C_MODULE_NAME
-#define RLBOX_WASM2C_MODULE_NAME rlboxsoundtouch
 
 #ifdef MOZ_WASM_SANDBOXING_SOUNDTOUCH
 // Include the generated header file so that we are able to resolve the symbols
 // in the wasm binary
-#  include "rlboxsoundtouch.wasm.h"
+#  include "rlbox.wasm.h"
 #  define RLBOX_USE_STATIC_CALLS() rlbox_wasm2c_sandbox_lookup_symbol
 #  include "mozilla/rlbox/rlbox_wasm2c_sandbox.hpp"
 #else
@@ -45,36 +43,21 @@ namespace mozilla {
 
 class RLBoxSoundTouch {
  public:
-  RLBOX_SOUNDTOUCH_API
   RLBoxSoundTouch();
-  RLBOX_SOUNDTOUCH_API
   bool Init();
-  RLBOX_SOUNDTOUCH_API
   ~RLBoxSoundTouch();
 
-  RLBOX_SOUNDTOUCH_API
   void setSampleRate(uint aRate);
-  RLBOX_SOUNDTOUCH_API
   void setChannels(uint aChannels);
-  RLBOX_SOUNDTOUCH_API
   void setPitch(double aPitch);
-  RLBOX_SOUNDTOUCH_API
   void setSetting(int aSettingId, int aValue);
-  RLBOX_SOUNDTOUCH_API
   void setTempo(double aTempo);
-  RLBOX_SOUNDTOUCH_API
   uint numChannels();
-  RLBOX_SOUNDTOUCH_API
   tainted_soundtouch<uint> numSamples();
-  RLBOX_SOUNDTOUCH_API
   tainted_soundtouch<uint> numUnprocessedSamples();
-  RLBOX_SOUNDTOUCH_API
   void setRate(double aRate);
-  RLBOX_SOUNDTOUCH_API
   void putSamples(const mozilla::AudioDataValue* aSamples, uint aNumSamples);
-  RLBOX_SOUNDTOUCH_API
   uint receiveSamples(mozilla::AudioDataValue* aOutput, uint aMaxSamples);
-  RLBOX_SOUNDTOUCH_API
   void flush();
 
  private:
@@ -85,7 +68,6 @@ class RLBoxSoundTouch {
   uint mSampleBufferSize{1};
   tainted_soundtouch<soundtouch::SoundTouch*> mTimeStretcher{nullptr};
 
-  RLBOX_SOUNDTOUCH_API
   void resizeSampleBuffer(uint aNewSize);
 };
 
