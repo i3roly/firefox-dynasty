d30bda75ef13d1105747f49a9a15722b1355ea2b
broly <broly@mac.com>
2025-02-10 18:23:50 -0700

    fix crashreporter's weak referencing issue for < 10.8 systems (central)
    markus (@mstange) knew the problem the entire time but thought it was best i struggle
    so that i truly understand why 10.7 is not as stable as it should be.
    
    it turns out the crashreporter binary's reason for crashing ("weak reference bla bla")
    was hinting at the larger issue: NSWindow does not allow weak references
    on sub 10.8 systems, which is likely the reason for the remaining
    issues on 10.7.
    
    for example you can browse fine but if you close all windows and then
    create a new one, sometimes it works, sometimes it won't and will crash
    with an EXC_BAD_ACCESS, which is the tell-tale sign of zeroing a weak
    reference.

--- toolkit/crashreporter/client/app/src/ui/macos/mod.rs
+++ toolkit/crashreporter/client/app/src/ui/macos/mod.rs
@@ -586,15 +586,23 @@ impl WindowRenderer {
             // Don't release windows when closed: we retain windows at the top-level.
             nswindow.setReleasedWhenClosed_(runtime::NO);
 
-            if let Some(close) = close {
-                let nswindow = nswindow.weak();
-                close.subscribe(move |&()| {
-                    if let Some(nswindow) = nswindow.lock() {
-                        nswindow.close();
-                    }
-                });
+            if macos_kernel_major_version() > Ok(MACOS_KERNEL_MAJOR_VERSION_LION) {
+                if let Some(close) = close {
+                    let nswindow = nswindow.weak();
+                    close.subscribe(move |&()| {
+                        if let Some(nswindow) = nswindow.lock() {
+                            nswindow.close();
+                        }
+                    });
+                }
+            } else {
+                if let Some(close) = close {
+                    let nswindow = nswindow.clone();
+                    close.subscribe(move |&()| {
+                            nswindow.close();
+                    });
+                }
             }
-
             if let Some(e) = content {
                 // Use an NSBox as a container view so that the window's content can easily have
                 // constraints set up relative to the parent (they can't be set relative to the
@@ -966,7 +974,9 @@ fn render_element(
              } else {
                 let parent: cocoa::NSSplitView = parent.try_into().unwrap();
                 unsafe {
-                    if macos_kernel_major_version() >= Ok(MACOS_KERNEL_MAJOR_VERSION_LION) {
+                    // it's uglier with subtreeifneeded on lion because textcontainer setSize
+                    // isn't valid, so we only call it on 10.8 for now
+                    if macos_kernel_major_version() > Ok(MACOS_KERNEL_MAJOR_VERSION_LION) {
                         parent.layoutSubtreeIfNeeded();
                     }
                     parent.addSubview_(child)
@@ -1020,7 +1030,9 @@ fn render_element(
              } else {
                 let parent: cocoa::NSSplitView = parent.try_into().unwrap();
                 unsafe {
-                    if macos_kernel_major_version() >= Ok(MACOS_KERNEL_MAJOR_VERSION_LION) {
+                    // it's uglier with subtreeifneeded on lion because textcontainer setSize
+                    // isn't valid, so we only call it on 10.8 for now
+                    if macos_kernel_major_version() > Ok(MACOS_KERNEL_MAJOR_VERSION_LION) {
                         parent.layoutSubtreeIfNeeded();
                     }
                     parent.addSubview_(child)
@@ -1150,10 +1162,12 @@ fn render_element(
                 }
                 {
                     let container = tv.textContainer();
-                    container.setSize_(cocoa::NSSize {
-                        width: f64::MAX,
-                        height: f64::MAX,
-                    });
+                        if macos_kernel_major_version() > Ok(MACOS_KERNEL_MAJOR_VERSION_LION) {
+                        container.setSize_(cocoa::NSSize {
+                            width: f64::MAX,
+                            height: f64::MAX,
+                        });
+                    }
                     container.setWidthTracksTextView_(runtime::YES);
                 }
                 if let Some(placeholder) = placeholder {
